c18b30f29372801416b77f901a5cd216318dc239
Trent <tnabrams64@gmail.com>
2019-11-20 20:42:55 -0600

    Fixed bugs and output

--- alu_v2.v
+++ alu_v2.v
@@ -249,15 +249,38 @@ module Mux_2 (input [15:0] in1, in2, input selector, output[15:0] out);
 		
 endmodule
 
+//DR. Becker was here. Bwa-ha-ha-HAH!
+//Mux with 2 bit selector for 16 bit input/output. 
+//If selector is 00, in1 is chosen. 
+//If selector is 01, in2 is chosen.
+//If selector is 10, in3 is chosen.
+//If selector is 11, in4 is chosen.
+//
+//
+module Mux_4 (input [15:0] in1, in2,in3,in4, input s1,s0, output[15:0] out);
 
-module SixteenBit_ALU(input [15:0] a, b, acc, input [5:0] opcode, output reg [31:0] out, output reset);
+	reg [4:0] i;
+	reg [15:0] out;
+	always @(*) begin
+		for (i = 0; i < 16; i = i + 1)
+			out[i] = (~s1&~s0) & in1[i] 
+			|        (~s1& s0) & in2[i] 
+			|        ( s1&~s0) & in3[i]
+			|        ( s1& s0) & in4[i];
+	end
+		
+endmodule
+
+
+module SixteenBit_ALU(input [15:0] a, b, acc, input [5:0] opcode, output reg [31:0] out, output reset, err);
 
 	wire [15:0] SR, SL, SUM, SUB, DIV, AND, OR, NOT, XOR;
 	wire [31:0] MULT, EXP, FACT;
 	wire [15:0] SR_ACC, SL_ACC, SUM_ACC, SUB_ACC, DIV_ACC, AND_ACC, OR_ACC, NOT_ACC, XOR_ACC;
 	wire [31:0] MULT_ACC, EXP_ACC, FACT_ACC;
-	wire err;
-	reg reset;
+	wire ERR_SUM, ERR_SUB, ERR_DIV, ERR_FACT, ERR_EXP, ACC_ERR_SUM, ACC_ERR_SUB, ACC_ERR_DIV, ACC_ERR_FACT, ACC_ERR_EXP;
+	reg reset, err;
+	reg load;//to handle flipflop
 	
 	Not_16 n(a, NOT);
 	Or_16 o(a, b, OR);
@@ -265,12 +288,12 @@ module SixteenBit_ALU(input [15:0] a, b, acc, input [5:0] opcode, output reg [31
 	Xor_16 xory(a, b, XOR);
 	ShiftLeft sl(a, SL);
 	ShiftRight sr (a, SR);
-	Add_16 add(a,b,err,SUM);
-	Subtract_16 sub(a,b,err,SUB);
+	Add_16 add(a,b,ERR_SUM,SUM);
+	Subtract_16 sub(a,b,ERR_SUB,SUB);
 	Multiply_16 mult(a,b,MULT);
-	Divide_16 div(a,b,err,DIV);
-	Fact_16 fact(a, err, FACT);
-	Exp_16 exp(a, err, EXP);
+	Divide_16 div(a,b,ERR_DIV,DIV);
+	Fact_16 fact(a, ERR_FACT, FACT);
+	Exp_16 exp(a, ERR_EXP, EXP);
 	
 	
 	Not_16 n_acc(acc, NOT_ACC);
@@ -279,12 +302,12 @@ module SixteenBit_ALU(input [15:0] a, b, acc, input [5:0] opcode, output reg [31
 	Xor_16 xory_acc(acc, b, XOR_ACC);
 	ShiftLeft sl_acc(acc, SL_ACC);
 	ShiftRight sr_acc (acc, SR_ACC);
-	Add_16 add_acc(acc,b,err,SUM_ACC);
-	Subtract_16 sub_acc(acc,b,err,SUB_ACC);
+	Add_16 add_acc(acc,b,ACC_ERR_SUM,SUM_ACC);
+	Subtract_16 sub_acc(acc,b,ACC_ERR_SUB,SUB_ACC);
 	Multiply_16 mult_acc(acc,b,MULT_ACC);
-	Divide_16 div_acc(acc,b,err,DIV_ACC);
-	Fact_16 fact_acc(acc, err, FACT_ACC);
-	Exp_16 exp_acc(acc, err, EXP_ACC);
+	Divide_16 div_acc(acc,b,ACC_ERR_DIV,DIV_ACC);
+	Fact_16 fact_acc(acc, ACC_ERR_FACT, FACT_ACC);
+	Exp_16 exp_acc(acc, ACC_ERR_EXP, EXP_ACC);
 	
 	
 	always @(*) begin
@@ -299,17 +322,17 @@ module SixteenBit_ALU(input [15:0] a, b, acc, input [5:0] opcode, output reg [31
 			//<<
 			5'd3 : out = SL;
 			//!
-			5'd4 : out = FACT;
+			5'd4 : begin out = FACT; err = ERR_FACT; end
 			//e^x
-			5'd5 : out = EXP;
+			5'd5 : begin out = EXP; err = ERR_EXP; end
 			//+
-			5'd6 : out = SUM;
+			5'd6 : begin out = SUM; err = ERR_SUM; end
 			//-
-			5'd7 : out = SUB;
+			5'd7 : begin out = SUB; err = ERR_SUB; end
 			//x
 			5'd8 : out = MULT;
 			//divide
-			5'd9 : out = DIV;
+			5'd9 : begin out = DIV; err = ERR_DIV; end
 			//and
 			5'd10 : out = AND;
 			//or
@@ -324,23 +347,25 @@ module SixteenBit_ALU(input [15:0] a, b, acc, input [5:0] opcode, output reg [31
 			//<<
 			5'd15 : out = SL_ACC;
 			//!
-			5'd16 : out = FACT_ACC;
+			5'd16 : begin out = FACT_ACC; err = ACC_ERR_FACT; end
 			//e^x
-			5'd17 : out = EXP_ACC;
+			5'd17 : begin out = EXP_ACC; err = ACC_ERR_EXP; end
 			//+
-			5'd18 : out = SUM_ACC;
+			5'd18 : begin out = SUM_ACC; err = ACC_ERR_SUM; end
 			//-
-			5'd19 : out = SUB_ACC;
+			5'd19 : begin out = SUB_ACC; err = ACC_ERR_SUB; end
 			//x
 			5'd20 : out = MULT_ACC;
 			//divide
-			5'd21 : out = DIV_ACC;
+			5'd21 : begin out = DIV_ACC; err = ACC_ERR_DIV; end
 			//and
 			5'd22 : out = AND_ACC;
 			//or
 			5'd23 : out = OR_ACC;
 			//xor
 			5'd24 : out = XOR_ACC;
+			//No-op
+			5'd25 : out = acc;
 		endcase
 	end
 endmodule
@@ -358,18 +383,29 @@ module testbench();
 	
  	wire [31:0] out;
 	reg clock;
-	wire reset;
+	wire reset, error;
 
-	Mux_2 MuxA(inA, 16'b0, reset, MuxOutA);
-	Mux_2 MuxB(inB, 16'b0, reset, MuxOutB);
+	/////////////////////////////////////////////////////////
+    //         load
+	//              reset
+	//                    feedback
+	//                          unused
+	//                                 s1
+	//                                      s0
+	//                                              output
+	//         in1  in2   in3   in4
+	//         noop   load  reset  reset
+	Mux_4 MuxA(a.out, inA, 16'b0, 16'b0, reset, ALU.load, MuxOutA);
+	Mux_4 MuxB(b.out, inB, 16'b0, 16'b0, reset, ALU.load, MuxOutB);
 	Mux_2 MuxAcc(out, 16'b0, reset, MuxOutAcc);
 	DFF a(clock, MuxOutA, outA);
 	DFF b(clock, MuxOutB, outB);
 	DFF acc(clock, MuxOutAcc, outAcc);
 
-	SixteenBit_ALU ALU(outA,outB,outAcc,opcode,out,reset);
+	SixteenBit_ALU ALU(outA,outB,outAcc,opcode,out,reset,error);
 	
 	initial begin
+		clock=1'b0;
 		forever begin
 			#5
 			clock = 1'b0;
@@ -381,39 +417,89 @@ module testbench();
 	//Reset is handled by the ALU. No need to have it as a stimulus!
 	//When you set the input for an operation, the input MUST be set the clock cycle BEFORE to give the clocks a chance to set them.
 	initial begin
-		#2
-		opcode = 5'd0;  //Clear
+		#1
+		opcode = 5'd0;  //Clear //t=1
+		//#0
+		#10
+		opcode =5'd25; //Default		
 		
-		#20
-		inA = 5'd3;
-		inB = 5'd2;
+		ALU.load=1'b1;  //t=6
+		#10
+		inA = 5'd3; //t=16
+		#10
+		inB = 5'd2;//t=26
+		#10//t=36
+		ALU.load=1'b0;
+		#10//t=46
+		
+		
+		
 		opcode = 5'd6;	//A + B
+		#10
 		
-		#20
+		
 		opcode = 5'd14;	//SR Acc
+		#10
 		
-		#20
-		opcode = 5'd14;	//Acc!
+		opcode=5'd25; //No-op
+		#10 
 		
-		#20
+		opcode = 5'd14;	//SR Acc!
+		#10
+		
+		opcode=5'd25; //No-op
+		#10 	
+		
 		opcode = 5'd13;	//Negate the result
+		#10
 		
-		#20
 		opcode = 5'd0;	//Clear
+		#10
 		
-		#20
 		opcode = 5'd12;	//Bitwise XOR 3 and 2
+		#10
 		
-		#20
+		opcode = 5'd25;	//No-op
+		ALU.load = 1'b1;
+		#10
+		inA = 5'd3;
+		#10
+		inB = 5'd20;
+		#10
+		ALU.load = 1'b0;
+		#10
+		
+		opcode = 5'd7;
+		#10
+		
+		opcode = 5'd17;
+		#10
+		
+		opcode = 5'd25;
+		ALU.load = 1'b1;
+		#10
+		inA = 5'd20;
+		#10
+		ALU.load = 1'b0;
+		#10
+		
+		opcode = 5'd5;
+		#10
+		
+		
+		
 		$finish;
 	end
 
-	initial begin
-		#1
-		$display("Input A          Input B          Accumulator      Opcode Output                           ");
+	initial begin	
+		#3
+	
+		$display("Input A          Input B          Accumulator      Opcode Output                           Error");
 		forever begin
-			#20
-			$display("%1b %1b %1b %1b %1b", outA, outB, outAcc, opcode, out);
+			#10
+	
+			//DR. BECKER SAY HI! $display("%1b %1b %1b %1b %5d %5d",clock,reset,ALU.reset,ALU.load,a.out,b.out);
+			$display("%1b %1b %1b %1b %1b %1b",outA, outB, outAcc, opcode, out, error);
 		end
 	end
 endmodule
